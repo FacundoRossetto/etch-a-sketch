f009277b2596e46fed247c0866513e9a1843aeed
JaguarDrinks <facundorossetto@gmail.com>
2023-12-19 17:11:54 +0100

    Size button Now, that button on the header, shows a pop-up when pressed, which will allow us to adjust de grid size later.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let grid = document.getElementById("grid")
+let sizeBtn = document.getElementById("sizeBtn")
+let popUp = document.getElementById("pop-up")
 
 for (var i = 0; i < 256; i++) {
 let square = document.createElement("div")
@@ -9,3 +11,6 @@ square.style.border = "1px solid black"
 grid.appendChild(square)
 }
 
+sizeBtn.addEventListener("click", ()=> {
+    popUp.classList.toggle("hidden")
+})
