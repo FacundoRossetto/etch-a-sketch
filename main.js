4b7c1f58da16d2e69a788194e5f548cde1a27c52
JaguarDrinks <facundorossetto@gmail.com>
2023-12-19 17:00:01 +0100

    Squares hovering Setup a class for hover on the squares

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let grid = document.getElementById("grid")
 
 for (var i = 0; i < 256; i++) {
 let square = document.createElement("div")
+square.classList = "paint"
 square.style.width = "30px"
 square.style.height = "30px"
 square.style.border = "1px solid black"
